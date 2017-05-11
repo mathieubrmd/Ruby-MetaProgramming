47da75771d9978a7becd6719f89b81fea5d03e7c
mathieubourmaud <mathieu.bourmaud@gmail.com>
2017-05-11 14:01:29 +0200

    Headers

---
+++ array_extension.rb
@@ -0,0 +1,4 @@
+##
+#  Mathieu Bourmaud - 19941124-P335
+#  Martin Porrès - 19940926-P170
+##
---
+++ code_generation.rb
@@ -0,0 +1,8 @@
+##
+#  Mathieu Bourmaud - 19941124-P335
+#  Martin Porrès - 19940926-P170
+##
+
+class Model
+
+end
