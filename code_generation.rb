e10220418748c12ed35d620ac8849175fa59e550
mathieubourmaud <mathieu.bourmaud@gmail.com>
2017-05-11 14:25:46 +0200

    Module Model created. When the filename is passed as parameter, it generates a string from it that is formatted like so : toto.text -> Toto

--- code_generation.rb
+++ code_generation.rb
@@ -3,6 +3,31 @@
 #  Martin Porrès - 19940926-P170
 ##
 
-class Model
+module Model
+
+  # Take a filename as argument
+  # Based on the content of the file, it will generate a class with attributes and getters/setters
+  # It returns an array of the generated class
+  def Model.load_from_file(filename)
+    Model.create_class(Model.get_classname_from_file(filename))
+  end
+
+  # Generate the class
+  def Model.create_class(name)
+    printf("%s\n", name)
+  end
+
+  # This method takes the filename as argument
+  # It returns the class name : From toto.txt to Toto
+  def Model.get_classname_from_file(filename)
+
+    class_name = filename.split('.').first
+    class_name = class_name.capitalize
+
+    return class_name
+  end
 
 end
+
+
+Model.load_from_file("toto.txt")
