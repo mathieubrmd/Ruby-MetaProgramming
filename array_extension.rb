0542bd55bc7954e3b44ba2a4245ed0a1e6cc7790
mathieubourmaud <mathieu.bourmaud@gmail.com>
2017-05-14 16:05:20 +0200

    Basic array_extension implementation

--- array_extension.rb
+++ array_extension.rb
@@ -1,4 +1,17 @@
 ##
 #  Mathieu Bourmaud - 19941124-P335
-#  Martin Porrès - 19940926-P170
 ##
+
+class Array
+  def select_first(attr)
+    attr_name = attr.keys[0]
+    attr_values = attr.values[0]
+
+    self.each do | x |
+      if eval("x.#{attr_name} == '#{attr_values}'")
+        return x
+      end
+    end
+
+  end
+end
